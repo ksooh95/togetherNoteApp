c2b103b1e82911fef2a5a9f359e4601564b41b0d
김수환 <kimsoohwan@gimseoghwan-ui-MacBookAir.local>
2024-01-31 16:31:26 +0900

    together v0.2

--- js/detail.js
+++ js/detail.js
@@ -1,10 +1,11 @@
 let dMemo = document.querySelector('.detail_memo');
 let dTitle = document.querySelector('.detail_title');
 let dText = document.querySelector('.detail_text');
+let editBtn = document.querySelector('#editBtn');
 
 let detailData = localStorage.getItem('data');
 detailData = JSON.parse(detailData);
-console.log(detailData);
+console.log('detailData :', detailData);
 
 const thisId = window.location.search.slice(2);
 console.log('지금 아이디 :', thisId);
@@ -16,14 +17,25 @@ const real = detailData.find((e) => {
 console.log('아이디 똑같은거 가져옴 :', real);
 
 const dtailMemo = () => {
-
-dTitle.value =  real.title
-dText.value = real.text
+    dTitle.value = real.title;
+    dText.value = real.text;
 };
 
-
-    
 window.onload = () => {
     dtailMemo();
 };
+
+editBtn.addEventListener('click', () => {
+    real.title = dTitle.value;
+    real.text = dText.value;
+    localStorage.setItem('data', JSON.stringify(detailData));
+});
+
+// 수정설명
+// editBtn <-- 수정버튼임 이거 눌렀을때 디테일페이지에 뿌려줄때 하는것처럼
+//  real.title에 dTitle.value , real.text에 dTitle.value를 넣어준다음 로컬스토리지에 다시 저장해주면됨!!
+// 되긴하는데 확인필요 !! 공부하기 !!
+
+
+
 
--- js/popup.js
+++ js/popup.js
@@ -3,23 +3,41 @@ let modal = document.querySelector('.black-bg');
 let deleteBtn = document.querySelector('#deleteBtn');
 let closeBtn = document.querySelector('.close');
 
-
 deleteOpen.addEventListener('click', () => {
-    modal.classList.add('show-modal')
-})
+    modal.classList.add('show-modal');
+});
 
 modal.addEventListener('click', (e) => {
-    if(e.target === modal){
-        modal.classList.remove('show-modal')
+    if (e.target === modal) {
+        modal.classList.remove('show-modal');
     }
-})
+});
 
 closeBtn.addEventListener('click', () => {
-    modal.classList.remove('show-modal')
-})
+    modal.classList.remove('show-modal');
+});
 
 deleteBtn.addEventListener('click', () => {
-    modal.classList.remove('show-modal')
-    location.href = 'index.html'
-})
+    detailData.map((e, index) => {
+        if (e.id.toString() === thisId) {
+            console.log('여기까지 왔음');
+            detailData.splice(index, 1);
+            localStorage.setItem('data', JSON.stringify(detailData));
+        }
+    });
+    console.log('삭제버튼 누름');
+    // console.log('deleteBtn :', deleteBtn);
+    // console.log('detailData :', detailData);
+    // console.log('thisId', typeof thisId);
 
+    modal.classList.remove('show-modal');
+    location.href = 'index.html';
+});
+
+//삭제 설명
+// 1. 삭제 버튼 눌렀을때 localStorage로 저장했던 data를 가져온 detailData를 map으로 돌린다!!
+//  e, i의 의미는 e는 그냥 이름지은거!! index는 몇번째인지 찾는거!!
+
+// 2. index로 몇번째인지 찾는 방법은 //https://ordinary-code.tistory.com/172 이곳을 참고했다!
+// e.id.toString() === thisId 로 같은 아이디인걸 찾은것임!! detail페이지와 같음 !
+// 그다음 detailData.splice() 로 내가 찾은것을 배열에서 없애준다음 로컬스토리지에 setItem 해주었다 !
